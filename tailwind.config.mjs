a4287ea0e0c6aa289459a9a4405ebefdd22a8ce9
NaHuynh <hhuynh22@gmu.edu>
2025-10-30 11:09:24 -0400

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // pick any daisyUI themes you want
+  },
+};
